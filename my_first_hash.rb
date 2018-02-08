7c745346e7c1bb2975a6261aefafea1c84ccc7b7
Neil Seejoor <nvseejoor@gmail.com>
2018-02-08 19:03:07 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -19,11 +19,7 @@ the_manifest = {
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => "5",
-                  "porcelain vases" => "2",
-                  "oil paintings" => "3"
-                 }
+  shipping_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
